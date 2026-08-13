b9db4de5728dc02c82798ed81d3cfb4806fc7e38
c-dez <c-dez@proton.me>
2026-08-12 23:02:29 -0600

    empezando first person camera

--- gameplay/characters/veicles/ball_2/camera.gd
+++ gameplay/characters/veicles/ball_2/camera.gd
@@ -1,9 +1,10 @@
 extends SpringArm3D
 class_name Camera
 
-@onready var mesh:MeshInstance3D = owner.get_node("Mesh")
-@export var hight_offset:float = 1.0
-@export var weight:float = 20.0
+@onready var mesh: MeshInstance3D = owner.get_node("Mesh")
+@export var hight_offset: float = 1.0
+@export var weight: float = 20.0
+var _offset: float = 0
 
 
 func _ready() -> void:
@@ -13,22 +14,25 @@ func _ready() -> void:
 func _process(delta: float) -> void:
     _set_camera_position(delta)
 
-var offset:float = 0
-func _set_camera_position(delta)->void:
+func _set_camera_position(delta) -> void:
     global_position = Vector3(mesh.global_position.x, mesh.global_position.y + hight_offset, mesh.global_position.z)
+
     var lateral = owner.linear_velocity.dot(mesh.global_basis.x)
 
-    offset = lerp(
+    _offset = lerp(
         0.0,
-        lateral * 0.8 ,
-        4* delta
+        lateral * 0.8,
+        4 * delta
     )
 
-    var camera_target:= mesh.rotation.y 
+    var camera_target := mesh.rotation.y
+
     rotation.y = lerp_angle(
         rotation.y,
-        camera_target + offset,
-        weight*delta
+        camera_target + _offset,
+        weight * delta
     )
+
+    
     
     pass
