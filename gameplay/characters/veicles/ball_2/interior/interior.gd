bc24df5625ebb7bd3fc1c0a79a9720e5ec680bc3
c-dez <c-dez@proton.me>
2026-08-13 19:50:25 -0600

    ajustado posicion de rearCamera

--- gameplay/characters/veicles/ball_2/interior/interior.gd
+++ gameplay/characters/veicles/ball_2/interior/interior.gd
@@ -1,13 +1,12 @@
 extends Node3D
 
 @onready var rear_camera:Camera3D = $SubViewport/RearCamera
+@onready var marker:Marker3D = $Marker3D
 
-@onready var mirror:MeshInstance3D = $mirror
 
 func _process(_delta: float) -> void:
-    rear_camera.global_basis = mirror.global_basis
-    rear_camera.global_position = mirror.global_position
-    # rear_camera.rotation.x = 0
+    rear_camera.global_basis = marker.global_basis
+    rear_camera.global_position = marker.global_position
     rear_camera.global_basis.z *= -1
 
     pass
