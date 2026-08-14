c4e774563920bdabe302f7f44f6c589f76c822a7
c-dez <c-dez@proton.me>
2026-08-13 19:10:46 -0600

    ajustado retrovisor

--- gameplay/characters/veicles/ball_2/interior/interior.gd
+++ gameplay/characters/veicles/ball_2/interior/interior.gd
@@ -4,7 +4,7 @@ extends Node3D
 
 @onready var mirror:MeshInstance3D = $mirror
 
-func _process(delta: float) -> void:
+func _process(_delta: float) -> void:
     rear_camera.global_basis = mirror.global_basis
     rear_camera.global_position = mirror.global_position
     # rear_camera.rotation.x = 0
